aea1997233bf63394d3108f0de31dc8e30637559
Harshit Manchanda <128061019+harshit6239@users.noreply.github.com>
2024-04-16 13:39:12 +0530

    created utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running`);
+    })
+    app.on("error", (error)=>{
+        console.log(`Express server error: ${error}`);
+    })
+})
+.catch((error)=>{
+
+});
